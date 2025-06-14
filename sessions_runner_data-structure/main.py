49fd2131aac74f7839c5bc949239c6efe702a10c
Paurakh <pandeypaurakhraj@gmail.com>
2025-06-14 23:58:39 +0545

    session switching

--- sessions_runner_data-structure/main.py
+++ sessions_runner_data-structure/main.py
@@ -6,7 +6,6 @@ import asyncio
 from dotenv import load_dotenv
 from icecream import ic
 from google.adk.sessions import Session
-# Import the grounding handler
 from question_answer_agent.grounding_handler import GroundingMetadataHandler
 
 load_dotenv()
@@ -40,6 +39,31 @@ state = {
     ]
 }
 
+state_2 =  {
+    "Username": "JohnDoe",
+    "Email": "jhonbabu@gmail.com",
+    "FullName": "John Babu Doe",
+    "Age": 27,
+    "Gender": "Male",
+    "Location": "San Francisco, USA",
+    "PreferredLanguage": "English",
+    "MembershipStatus": "Platinum",
+    "AccountCreated": "2020-03-18",
+    "LastLogin": "2025-06-13 09:20:45",
+    "Interests": ["Travel", "Photography", "Adventure Sports", "Cultural Exploration"],
+    "Occupation": "Travel Blogger",
+    "DailyActiveHours": "4-8 hours",
+    "SupportTier": "Platinum Elite",
+    "SubscriptionRenewalDate": "2026-03-18",
+    "DevicesUsed": ["Samsung Galaxy S24 Ultra", "Surface Laptop 6", "GoPro Hero 12"],
+    "PreferredContactMethod": "Phone",
+    "RecentActivity": [
+        "Booked: 7-Day Japan Cherry Blossom Tour",
+        "Reviewed: 'The Ritz-Carlton, Kyoto'",
+        "Searched: 'Top Hiking Trails in New Zealand'"
+    ]
+}
+
 async def create_session() -> InMemorySessionService:
     session_service = InMemorySessionService()
     await session_service.create_session(
@@ -49,13 +73,13 @@ async def create_session() -> InMemorySessionService:
         state=state,
     )
     
-    #  test session
+    #session_2
     
     await session_service.create_session(
         app_name=APP_NAME,
         user_id=USER_ID,
-        session_id="test_session_123",
-        state={"test_key": "test_value"},
+        session_id="session_2_ID",
+        state=state_2,
     )
     
     return session_service
@@ -89,6 +113,7 @@ async def call_agent(runner: Runner, user_input: str, session_id, user_id) -> di
     }
 
 async def main():
+    global SESSION_ID, USER_ID, APP_NAME
     try:
         session_service = await create_session()
         runner = Runner(
@@ -112,28 +137,44 @@ async def main():
         user_input = input("\n🤔 You: ")
         if user_input.lower() in ["exit", "quit"]:
             print("👋 Exiting the session.")
-            retrieved_session = await session_service.get_session(app_name=APP_NAME,
-                                                         user_id=USER_ID,
-                                                         session_id = SESSION_ID)
+            # retrieved_session = await session_service.get_session(app_name=APP_NAME,
+            #                                              user_id=USER_ID,
+            #                                              session_id = SESSION_ID)
             # ic(retrieved_session.events
             # )
-            ic(session_service.__dict__)
-            ic(session_service.sessions)
-            list_of_existing_sessions = await session_service.list_sessions(
-                app_name=APP_NAME,
-                user_id=USER_ID,
-            )
-            ic(list_of_existing_sessions)
-            ic(list_of_existing_sessions.sessions)
-            ic(list_of_existing_sessions.sessions[-1].id)
-            get_specific_session = await session_service.get_session(
-                app_name=APP_NAME,
-                user_id=USER_ID,
-                session_id=list_of_existing_sessions.sessions[-1].id
-            )
-            ic(get_specific_session)
+            # ic(session_service.__dict__)
+            # ic(session_service.sessions)
+            # list_of_existing_sessions = await session_service.list_sessions(
+            #     app_name=APP_NAME,
+            #     user_id=USER_ID,
+            # )
+            # ic(list_of_existing_sessions)
+            # ic(list_of_existing_sessions.sessions)
+            # ic(list_of_existing_sessions.sessions[-1].id)
+            # get_specific_session = await session_service.get_session(
+            #     app_name=APP_NAME,
+            #     user_id=USER_ID,
+            #     session_id=list_of_existing_sessions.sessions[-1].id
+            # )
+            # ic(get_specific_session)
             break
-        
+        if user_input.lower() == "sessions":
+            
+            #Approach to continue with an exsiting session and switching into them
+            
+            existing_sessions= await session_service.list_sessions(
+                app_name=APP_NAME,
+                user_id=USER_ID,
+            )
+            for indx, session in enumerate(existing_sessions.sessions):
+                ic(indx,session.id)
+                
+            session_to_continue = input("Enter the session index to continue with: ")
+            SESSION_ID = existing_sessions.sessions[int(session_to_continue)].id
+            # re-ask user's prompt to proceed with after sessoin change
+                
+            user_input = input("\n🤔 You: ")
+
         try:
             response = await call_agent(
                 runner=runner, 
@@ -167,8 +208,7 @@ async def main():
             else:
                 print("ℹ️  No Google Search was used for this query.")
             
-            # Debug information (optional - remove in production)
-            if True:  # Set to True for debugging
+            if False:  # Set to True for debugging
                 ic(response)
             
         except Exception as e:
